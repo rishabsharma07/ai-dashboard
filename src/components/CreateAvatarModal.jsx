50c4c2cf3c1250ba2aa388d27e2babb5aec85159
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-05-15 00:10:12 +0530

    clean code

--- src/components/CreateAvatarModal.jsx
+++ src/components/CreateAvatarModal.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef} from 'react';
 import { X, Upload, User2 } from 'lucide-react';
 
 function CreateAvatarModal({ isOpen, onClose }) {
